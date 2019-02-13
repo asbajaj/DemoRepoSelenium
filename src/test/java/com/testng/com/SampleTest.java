7fb8927e8c3b9df0c45cb556fde7ff926c76894d
Ashish <ashishbajaj@live.com>
2019-02-13 13:48:16 +0530

    added new class

---
+++ src/test/java/com/testng/com/SampleTest.java
@@ -0,0 +1,13 @@
+package com.testng.com;
+
+public class SampleTest {
+
+	public void print()
+	 {
+		// TODO Auto-generated method stub
+System.out.println("My name is Ashish Bajaj");
+	}
+	
+
+}
+ 
